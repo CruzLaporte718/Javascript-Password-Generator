39a3314f6a81a977200cbf820739f5c7b379b7b4
Dennis Cruz <Dcruz198784@gmail.com>
2023-02-14 12:21:03 -0500

    added Assets file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+//create function and log it:
+function generatePassword(){
+    var numericCharacters = ['7','4','12'];
+    var upperCaseCharacters = ['M','J'];
+    console.log("7,4,12,M,J");
+
+
+//Password Criteria and length:
+function passwordLength(){
+    console.log("length");
+}
+
+
+   return "7,4,12,M,J";
+}
 //Write password to the #password input
 function writePassword(){
     var password = generatePassword();
